9d7987d48d29bead5cf2f82bda30469694f902c5
Renato Marino Henz <renatomarinohenz@gmail.com>
2021-03-13 18:04:45 -0300

    Inserção do 'cors' para utilização da API pela aplicação em React.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+// Habilitando a utilização da API para aplicações em React, etc.
+// Deve vir antes da utilizaçã odas rotas
+app.use(cors());
 app.use(routes);
 
 export default app;
